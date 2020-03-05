4d2cbd1a72c83788d2f1eb82c242a67495681357
Simon Mittag <simonmittag@gmail.com>
2020-03-05 11:11:49 +1100

    initial

---
+++ src/github.com/simonmittag/babyjabba/logger/logger.go
@@ -0,0 +1,17 @@
+package logger
+
+import (
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"os"
+)
+
+// Init sets up a global logger instance
+func Init() {
+	zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	w := zerolog.ConsoleWriter{
+		Out:     os.Stderr,
+		NoColor: false,
+	}
+	log.Logger = log.Output(w)
+}
---
+++ src/github.com/simonmittag/babyjabba/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"os"
+
+	"github.com/rs/zerolog/log"
+	"github.com/simonmittag/babyjabba/logger"
+)
+
+func main() {
+	logger.Init()
+	log.Debug().Str("TimeZone", os.Getenv("TZ")).Msg("BabyJabba is starting up..")
+
+	for {
+
+	}
+}
