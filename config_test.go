f0376715cb88a44242befa4de46ca0f841072ebd
Simon Mittag <simonmittag@gmail.com>
2020-04-28 09:09:43 +1000

    added test for running upstream http server

--- config_test.go
+++ config_test.go
@@ -14,6 +14,7 @@ func TestMain(m *testing.M) {
 
 func testSetup() {
 	os.Setenv("TZ", "Australia/Sydney")
+	setupJabbaWithMse6()
 }
 
 //TestDefaultDownstreamReadTimeout
--- server_test.go
+++ server_test.go
@@ -1,17 +1,84 @@
 package jabba
 
 import (
+	"fmt"
 	"net/http"
+	"sync"
 	"testing"
+	"time"
 )
 
-func TestServerBootStrap(t *testing.T) {
-	Boot.Add(1)
-	go BootStrap()
-	Boot.Wait()
+var mse6Wait sync.WaitGroup = sync.WaitGroup{}
 
+func TestServerBootStrap(t *testing.T) {
 	resp, _ := http.Get("http://localhost:8080/about")
 	if resp.StatusCode != 200 {
 		t.Errorf("server does not return ok status response after starting, want 200, got %v", resp.StatusCode)
+	}
+}
+
+func TestServerMakesSuccessfulUpstreamConnection(t *testing.T) {
+	resp, err := http.Get("http://localhost:8080/v2/billing")
+
+	if err!=nil {
+		t.Errorf("error connecting to upstream, cause: %v", err)
+	}
+
+	if resp.StatusCode != 200 {
+		t.Errorf("server does not return ok from working upstream, want 200, got %v", resp.StatusCode)
+	}
+}
+
+func setupJabbaWithMse6() {
+	//start mse6 our upstream server
+	mse6Wait.Add(1)
+	go mse6Listen()
+	mse6Wait.Wait()
+
+	//start jabba
+	Boot.Add(1)
+	go BootStrap()
+	Boot.Wait()
+}
+
+func mse6Listen() {
+	fmt.Println("starting Upstream http server MSE6")
+
+	http.HandleFunc("/v2/billing", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Encoding", "identity")
+		w.WriteHeader(200)
+		w.Write([]byte(`{"MSE6":"Hello from the billing endpoint"}`))
+	})
+
+	http.HandleFunc("/v2/posting", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Encoding", "identity")
+		w.WriteHeader(200)
+		w.Write([]byte(`{"MSE6":"Hello from the posting endpoint"}`))
+	})
+
+	http.HandleFunc("/v2/slowbody", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Encoding", "identity")
+		w.WriteHeader(200)
+		time.Sleep(time.Second*2)
+		w.Write([]byte(`{"MSE6":"Hello from the posting endpoint"}`))
+	})
+
+	http.HandleFunc("/v2/slowheader", func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(time.Second*2)
+		w.Header().Set("Content-Encoding", "identity")
+		w.WriteHeader(200)
+		w.Write([]byte(`{"MSE6":"Hello from the posting endpoint"}`))
+	})
+
+	http.HandleFunc("/v2/gzip", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Encoding", "gzip")
+		w.WriteHeader(200)
+		w.Write(Gzip([]byte(`{"MSE6":"Hello from the posting endpoint"}`)))
+	})
+
+	mse6Wait.Done()
+	err := http.ListenAndServe(":60083", nil)
+	if err != nil {
+		panic(err.Error())
 	}
 }
