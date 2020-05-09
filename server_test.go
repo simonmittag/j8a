7d6494ec51f04c7fff4c784fa23c7fcaf060ff0a
Simon Mittag <simonmittag@gmail.com>
2020-05-09 10:24:19 +1000

    removed mse6 from test setup, now using external project. TODO: call mse6 process from within go?

--- server_test.go
+++ server_test.go
@@ -1,11 +1,8 @@
 package jabba
 
 import (
-	"github.com/rs/zerolog/log"
 	"net/http"
-	"runtime/debug"
 	"testing"
-	"time"
 )
 
 func TestServerBootStrap(t *testing.T) {
@@ -16,7 +13,7 @@ func TestServerBootStrap(t *testing.T) {
 }
 
 func TestServerMakesSuccessfulUpstreamConnection(t *testing.T) {
-	resp, err := http.Get("http://localhost:8080/mse6/billing")
+	resp, err := http.Get("http://localhost:8080/mse6/get")
 
 	if err != nil {
 		t.Errorf("error connecting to upstream, cause: %v", err)
@@ -31,9 +28,6 @@ func TestServerUpstreamReadTimeoutFailsWithSlowHeader(t *testing.T) {
 	Runner.Connection.Upstream.ReadTimeoutSeconds = 1
 	httpClient = scaffoldHTTPClient(*Runner)
 	resp, err := http.Get("http://localhost:8080/mse6/slowheader")
-	debug.PrintStack()
-	time.Sleep(4*time.Second)
-	debug.PrintStack()
 
 	if err != nil {
 		t.Errorf("error connecting to upstream, cause: %v", err)
@@ -90,60 +84,6 @@ func TestServerUpstreamReadTimeoutPassesWithSlowBody(t *testing.T) {
 
 func setupJabbaWithMse6() {
 	Boot.Add(1)
-	go mse6Listen()
-
-	Boot.Add(1)
 	go BootStrap()
 	Boot.Wait()
-}
-
-func mse6Listen() {
-	log.Trace().Msg("starting Upstream http server MSE6")
-
-	http.HandleFunc("/mse6/billing", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Encoding", "identity")
-		w.WriteHeader(200)
-		w.Write([]byte(`{"MSE6":"Hello from the mse6/billing endpoint"}`))
-	})
-
-	http.HandleFunc("/mse6/posting", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Encoding", "identity")
-		w.WriteHeader(200)
-		w.Write([]byte(`{"MSE6":"Hello from the mse6/posting endpoint"}`))
-	})
-
-	http.HandleFunc("/mse6/slowbody", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Encoding", "identity")
-		//we must have this, else golang sets it to 'chunked' after 2nd write
-		w.Header().Set("Transfer-Encoding", "identity")
-		w.WriteHeader(200)
-		hj, _ := w.(http.Hijacker)
-		conn, bufrw, _ := hj.Hijack()
-		defer conn.Close()
-		time.Sleep(time.Second * 3)
-		bufrw.WriteString(`[{"mse6":"Hello from the mse6/slowbody endpoint"}`)
-		bufrw.Flush()
-		time.Sleep(time.Second * 3)
-		bufrw.WriteString(`,{"mse6":"and some more data from the mse6/slowbody endpoint"}]`)
-		bufrw.Flush()
-	})
-
-	http.HandleFunc("/mse6/slowheader", func(w http.ResponseWriter, r *http.Request) {
-		time.Sleep(time.Second * 3)
-		w.Header().Set("Content-Encoding", "identity")
-		w.WriteHeader(200)
-		w.Write([]byte(`{"MSE6":"Hello from the mse6/slowheader endpoint"}`))
-	})
-
-	http.HandleFunc("/mse6/gzip", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Encoding", "gzip")
-		w.WriteHeader(200)
-		w.Write(Gzip([]byte(`{"MSE6":"Hello from the mse6/gzip endpoint"}`)))
-	})
-
-	Boot.Done()
-	err := http.ListenAndServe(":60083", nil)
-	if err != nil {
-		panic(err.Error())
-	}
 }
