e44a4ed8af5c3611ca6ff2f0880b5adecb9edca9
Simon Mittag <simonmittag@gmail.com>
2020-05-20 08:57:02 +1000

    all tests for #27

--- integration/upstreamsockettimeout_test.go
+++ integration/upstreamsockettimeout_test.go
@@ -6,15 +6,79 @@ import (
 	"time"
 )
 
-func TestServerUpstreamConnectionTimeoutFailsWithBadDNS(t *testing.T) {
+//check integration test config file for correct values if test is failing
+func connectionTimeoutTestPeriod() int {
+	maxAttempts := 4
+	socketTimeout := 3
+	grace := 1
+	want := maxAttempts*socketTimeout + grace
+	return want
+}
+
+func TestServerUpstreamConnectionTimeoutFailsWithBadLocal(t *testing.T) {
 	start := time.Now()
-	resp, err := http.Get("http://localhost:8080/notdns")
+	resp, err := http.Get("http://localhost:8080/badlocal")
+	elapsed := time.Since(start)
+
+	want := connectionTimeoutTestPeriod()
+	if elapsed > time.Duration(want)*time.Second {
+		t.Errorf("upstream connection timeout exceed, want: %vs, got: %vs", want, elapsed)
+	}
+
+	if err != nil {
+		t.Errorf("error connecting to upstream, cause: %v", err)
+	}
+
+	if resp.StatusCode != 502 {
+		t.Errorf("upstream connection failure, want 502, got %v", resp.StatusCode)
+	}
+}
+
+func TestServerUpstreamConnectionTimeoutWithBadRemote(t *testing.T) {
+	start := time.Now()
+	resp, err := http.Get("http://localhost:8080/badremote")
+	elapsed := time.Since(start)
+
+	want := connectionTimeoutTestPeriod()
+	if elapsed > time.Duration(want)*time.Second {
+		t.Errorf("upstream connection timeout exceed, want: %vs, got: %vs", want, elapsed)
+	}
+
+	if err != nil {
+		t.Errorf("error connecting to upstream, cause: %v", err)
+	}
+
+	if resp.StatusCode != 502 {
+		t.Errorf("upstream connection failure, want 502, got %v", resp.StatusCode)
+	}
+}
+
+func TestServerUpstreamConnectionTimeoutWithBadIp(t *testing.T) {
+	start := time.Now()
+	resp, err := http.Get("http://localhost:8080/badip")
+	elapsed := time.Since(start)
+
+	want := connectionTimeoutTestPeriod()
+	if elapsed > time.Duration(want)*time.Second {
+		t.Errorf("upstream connection timeout exceed, want: %vs, got: %vs", want, elapsed)
+	}
+
+	if err != nil {
+		t.Errorf("error connecting to upstream, cause: %v", err)
+	}
+
+	if resp.StatusCode != 502 {
+		t.Errorf("upstream connection failure, want 502, got %v", resp.StatusCode)
+	}
+}
+
+func TestServerUpstreamConnectionTimeoutWithBadDNS(t *testing.T) {
+	start := time.Now()
+	resp, err := http.Get("http://localhost:8080/baddns")
 	elapsed := time.Since(start)
 
 	//check integration test config file for correct values if test is failing
-	maxAttempts:=4
-	socketTimeout:=3
-	want := maxAttempts*socketTimeout
+	want := connectionTimeoutTestPeriod()
 	if elapsed > time.Duration(want) * time.Second {
 		t.Errorf("upstream connection timeout exceed, want: %vs, got: %vs", want, elapsed)
 	}
