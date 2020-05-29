a76d4c1aa9d7819eb7b68ee72f5921298977f139
simon.mittag <simonmittag@gmail.com>
2020-05-30 07:00:07 +1000

    jabba now starts only for one bootstrap unit test, not for all the unit tests. it shouldn't even run during this phase, this is just for coverage testing

--- server_test.go
+++ server_test.go
@@ -6,6 +6,7 @@ import (
 )
 
 func TestServerBootStrap(t *testing.T) {
+	setupJabba()
 	resp, _ := http.Get("http://localhost:8080/about")
 	if resp.StatusCode != 200 {
 		t.Errorf("server does not return ok status response after starting, want 200, got %v", resp.StatusCode)
