122d47f55aa0fe0fcd4361018025fc7dc5e674e9
simon.mittag <simonmittag@gmail.com>
2020-06-23 07:45:12 +1000

    refactored test method to test wider array of conditions

--- integration/upstreamreadtimeout_test.go
+++ integration/upstreamreadtimeout_test.go
@@ -1,10 +1,11 @@
 package integration
 
 import (
+	"fmt"
 	"net/http"
 	"testing"
+	"time"
 )
-
 
 func TestServerMakesSuccessfulUpstreamConnection(t *testing.T) {
 	resp, err := http.Get("http://localhost:8080/mse6/get")
@@ -18,47 +19,51 @@ func TestServerMakesSuccessfulUpstreamConnection(t *testing.T) {
 	}
 }
 
-func TestServerUpstreamReadTimeoutFailsWithSlowHeader(t *testing.T) {
-	resp, err := http.Get("http://localhost:8080/mse6/slowheader?wait=4")
+func TestServer1UpstreamReadTimeoutFailsWithSlowHeader4S(t *testing.T) {
+	performJabbaTest(t, "/slowheader", 4, 12, 502, 8080)
+}
+
+func TestServer1UpstreamReadTimeoutFailsWithSlowBody4S(t *testing.T) {
+	performJabbaTest(t, "/slowbody", 4, 12, 502, 8080)
+}
+
+func TestServer1UpstreamReadTimeoutPassesWithSlowHeader2S(t *testing.T) {
+	performJabbaTest(t, "/slowheader", 2, 2, 200, 8080)
+}
+
+func TestServer1UpstreamReadTimeoutPassesWithSlowBody2S(t *testing.T) {
+	performJabbaTest(t, "/slowbody", 2, 2, 200, 8080)
+}
+
+func performJabbaTest(t *testing.T, testMethod string, wantUpstreamWaitSeconds int, wantTotalWaitSeconds int, wantStatusCode int, serverPort int) {
+	start := time.Now()
+	resp, err := http.Get(fmt.Sprintf("http://localhost:%d/mse6%s?wait=%d", serverPort, testMethod, wantUpstreamWaitSeconds))
+	gotTotalWait := time.Since(start)
+	gotStatusCode := resp.StatusCode
 
 	if err != nil {
-		t.Errorf("error connecting to upstream, cause: %v", err)
+		t.Errorf("error connecting to upstream for port %d, testMethod %s, cause: %v", serverPort, testMethod, err)
 	}
 
-	if resp.StatusCode != 502 {
-		t.Errorf("slow header writes! from server > upstream timeout should not return ok and fail after max attempts, want 502, got %v", resp.StatusCode)
+	if !okayTimeDrift(gotTotalWait, wantTotalWaitSeconds) {
+		t.Errorf("bad time drift for port %d, testMethod %s, want seconds %d, got %f", serverPort, testMethod, wantTotalWaitSeconds, gotTotalWait.Seconds())
+	}
+
+	if gotStatusCode != wantStatusCode {
+		t.Errorf("bad status code for port %d, testMethod %s, want statusCode %d, got %d", serverPort, testMethod, wantStatusCode, gotStatusCode)
 	}
 }
 
-func TestServerUpstreamReadTimeoutFailsWithSlowBody(t *testing.T) {
-	resp, err := http.Get("http://localhost:8080/mse6/slowbody?wait=4")
-	if err != nil {
-		t.Errorf("error connecting to upstream, cause: %v", err)
-	}
+func okayTimeDrift(elapsed time.Duration, waitSeconds int) bool {
+	fmin := 1.0
+	fmax := 1.1
+	elapsedSeconds := elapsed.Seconds()
 
-	if resp.StatusCode != 502 {
-		t.Errorf("slow body writes from server > upstream timeout should not return ok and fail after max attempts, want 502, got %v", resp.StatusCode)
+	if elapsedSeconds > fmax*float64(waitSeconds) {
+		return false
 	}
-}
-
-func TestServerUpstreamReadTimeoutPassesWithSlowHeader(t *testing.T) {
-	resp, err := http.Get("http://localhost:8080/mse6/slowheader?wait=2")
-	if err != nil {
-		t.Errorf("error connecting to upstream, cause: %v", err)
+	if elapsedSeconds < fmin*float64(waitSeconds) {
+		return false
 	}
-
-	if resp.StatusCode != 200 {
-		t.Errorf("slow header writes from server < upstream timeout should return ok, want 200, got %v", resp.StatusCode)
-	}
-}
-
-func TestServerUpstreamReadTimeoutPassesWithSlowBody(t *testing.T) {
-	resp, err := http.Get("http://localhost:8080/mse6/slowbody?wait=2")
-	if err != nil {
-		t.Errorf("error connecting to upstream, cause: %v", err)
-	}
-
-	if resp.StatusCode != 200 {
-		t.Errorf("slow body writes from server < upstream timeout should return ok, want 200, got %v", resp.StatusCode)
-	}
+	return true
 }
