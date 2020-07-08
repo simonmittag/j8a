d2f01be6e06e9599bf4850fc1cd9e08c12d81859
simon.mittag <simonmittag@gmail.com>
2020-07-08 10:31:37 +1000

    removed unwanted status message checks

--- integration/responsecode_test.go
+++ integration/responsecode_test.go
@@ -7,30 +7,26 @@ import (
 )
 
 func TestStatusCodeOfProxiedResponses100To200(t *testing.T) {
-	for i:=100;i<200;i++ {
-		performJabbaResponseCodeTest(t,i, i, 8080)
+	for i := 100; i < 200; i++ {
+		performJabbaResponseCodeTest(t, i, i, 8080)
 	}
 }
 
 func TestStatusCodeOfProxiedResponses200To400(t *testing.T) {
-	for i:=200;i<400;i++ {
-		performJabbaResponseCodeTest(t,i, i, 8080)
+	for i := 200; i < 400; i++ {
+		performJabbaResponseCodeTest(t, i, i, 8080)
 	}
 }
 
 func TestStatusCodeOfProxiedResponses500To599(t *testing.T) {
-	for i:=500;i<600;i++ {
-		performJabbaResponseCodeTest(t,i, 502, 8080)
+	for i := 500; i < 600; i++ {
+		performJabbaResponseCodeTest(t, i, 502, 8080)
 	}
 }
 
 func performJabbaResponseCodeTest(t *testing.T, getUpstreamStatusCode, wantDownstreamStatusCode int, serverPort int) {
 	resp, err := http.Get(fmt.Sprintf("http://localhost:%d/mse6/send?code=%d", serverPort, getUpstreamStatusCode))
 	gotDownstreamStatusCode := resp.StatusCode
-
-	//if wantDownstreamStatusCode>399&&wantDownstreamStatusCode<500 {
-	//	resp
-	//}
 
 	if err != nil {
 		t.Errorf("error connecting to upstream for port %d, /send, cause: %v", serverPort, err)
