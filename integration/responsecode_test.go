6d2a598e233873559f1a35eb80c365e1cfe18f69
simon.mittag <simonmittag@gmail.com>
2020-07-27 08:29:36 +1000

    more 100 continue tests

--- integration/responsecode_test.go
+++ integration/responsecode_test.go
@@ -12,7 +12,7 @@ import (
 )
 
 //TODO: once we start supporting max body size, this needs to be revisited. it sends 100 every time right now.
-func TestStatusCode100SentFromProxyWithPutIfExpected(t *testing.T) {
+func TestStatusCode100SentFromProxyWithPutIfExpected100Continue(t *testing.T) {
 	//step 1 we connect to Jabba with net.dial
 	c, err := net.Dial("tcp", ":8080")
 	if err != nil {
@@ -47,7 +47,42 @@ func TestStatusCode100SentFromProxyWithPutIfExpected(t *testing.T) {
 	}
 }
 
-func TestStatusCode200SentFromProxyWithPutIfExpected(t *testing.T) {
+func TestStatusCode100SentFromProxyWithPostIfExpected100Continue(t *testing.T) {
+	//step 1 we connect to Jabba with net.dial
+	c, err := net.Dial("tcp", ":8080")
+	if err != nil {
+		t.Errorf("unable to connect to jabba server for integration test, cause: %v", err)
+		return
+	}
+	defer c.Close()
+
+	//step 2 we send headers and terminate HTTP message.
+	checkWrite(t, c, "POST /mse6/post HTTP/1.1\r\n")
+	checkWrite(t, c, "Host: localhost:8081\r\n")
+	checkWrite(t, c, "Content-Type: application/json\r\n")
+	checkWrite(t, c, "Content-Length: 37\r\n")
+	checkWrite(t, c, "User-Agent: integration\r\n")
+	checkWrite(t, c, "Expect: 100-continue\r\n")
+	checkWrite(t, c, "Accept: */*\r\n")
+	checkWrite(t, c, "\r\n")
+	checkWrite(t, c, `{"key":"100 continue test request"}`+"\r\n")
+
+	//step 3 we try to read the server response. Warning this isn't a proper http client
+	//i.e. doesn't include parsing content length, reading response properly.
+	buf := make([]byte, 1024)
+	l, err := c.Read(buf)
+	t.Logf("jabba responded with %v bytes and error code %v", l, err)
+	t.Logf("jabba partial response: %v", string(buf))
+	if l == 0 {
+		t.Error("jabba did not respond, 0 bytes read")
+	}
+	response := string(buf)
+	if !strings.Contains(response, "100 Continue") {
+		t.Error("jabba did not respond with 100 continue")
+	}
+}
+
+func TestFinalStatusCode200SentFromProxyWithPutIfExpected100Continue(t *testing.T) {
 	client := &http.Client{}
 	serverPort := 8080
 	wantDownstreamStatusCode := 200
@@ -76,6 +111,38 @@ func TestStatusCode200SentFromProxyWithPutIfExpected(t *testing.T) {
 
 	if gotDownstreamStatusCode != wantDownstreamStatusCode {
 		t.Errorf("PUT with Expect: 100-continue did not result in OK, want %d, got %d", wantDownstreamStatusCode, gotDownstreamStatusCode)
+	}
+}
+
+func TestFinalStatusCode200SentFromProxyWithPostIfExpected100Continue(t *testing.T) {
+	client := &http.Client{}
+	serverPort := 8080
+	wantDownstreamStatusCode := 200
+
+	jsonData := map[string]string{"firstname": "Simon", "lastname": "Mittag", "rank": "Corporal"}
+	jsonValue, _ := json.Marshal(jsonData)
+	buf := bytes.NewBuffer(jsonValue)
+
+	url := fmt.Sprintf("http://localhost:%d/mse6/post", serverPort)
+	req, _ := http.NewRequest("POST", url, buf)
+
+	req.Header.Add("Expect", "100-continue")
+	req.Header.Add("Content-Type", "application/json")
+	resp, err := client.Do(req)
+	if resp != nil && resp.Body != nil {
+		defer resp.Body.Close()
+	}
+
+	gotDownstreamStatusCode := 0
+	if err != nil {
+		t.Errorf("error connecting to upstream for port %d, /send, cause: %v", serverPort, err)
+		return
+	} else {
+		gotDownstreamStatusCode = resp.StatusCode
+	}
+
+	if gotDownstreamStatusCode != wantDownstreamStatusCode {
+		t.Errorf("POST with Expect: 100-continue did not result in OK, want %d, got %d", wantDownstreamStatusCode, gotDownstreamStatusCode)
 	}
 }
 
