52e3b18655b3b2279510f86e488437f5067e7bd4
simon.mittag <simonmittag@gmail.com>
2020-11-19 09:53:03 +1100

    replaced jwt none token with no expiry

--- integration/jwt_test.go
+++ integration/jwt_test.go
@@ -11,7 +11,7 @@ func TestJwtNoneNotExpired(t *testing.T) {
 }
 
 func TestJwtNoneNoExpiry(t *testing.T) {
-	DoJwtTest(t, "/jwtnone", 200, "eyJ0eXAiOiJKV1QiLCJhbGciOiJub25lIn0.eyJpc3MiOiJqb2UiLCJodHRwOi8vZXhhbXBsZS5jb20vaXNfcm9vdCI6dHJ1ZSwianRpIjoiZTI4ZGY3MTItMGNmNy00ZTVlLWE3NjUtYjE0MGQwM2IwNDRjIiwiaWF0IjoxNjA1NjY3NDEzLCJleHAiOjE2MDU2NzEwMTN9.")
+	DoJwtTest(t, "/jwtnone", 200, "eyJ0eXAiOiJKV1QiLCJhbGciOiJub25lIn0.eyJpc3MiOiJqb2UiLCJodHRwOi8vZXhhbXBsZS5jb20vaXNfcm9vdCI6dHJ1ZSwianRpIjoiYjJjOTBiZDYtZmQ2Yy00MjI0LWI4MjItOGQ1ZWNlZWIzMmE0IiwiaWF0IjoxNjA1NzM5ODExfQ.")
 }
 
 func TestJwtNoneExpired(t *testing.T) {
