85ba20d03fa7f6d476ca53a5fa1cc5794f97a704
simon.mittag <simonmittag@gmail.com>
2020-11-19 08:22:13 +1100

    more nbf tests

--- integration/jwt_test.go
+++ integration/jwt_test.go
@@ -38,6 +38,19 @@ func TestJwtES256Expired(t *testing.T) {
 	DoJwtTest(t, "/jwtes256", 401, "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJpc3MiOiJqb2UiLCJodHRwOi8vZXhhbXBsZS5jb20vaXNfcm9vdCI6dHJ1ZSwianRpIjoiMjU4YzQ2NWMtYjg2OS00YjdlLWEyMzItMjFlMzEzNmY4YWJmIiwiaWF0IjoxNjA1NjQ3OTk2LCJleHAiOjE0NzU4NzgzNTd9.vWcnJ1pKN9bXUhFv34I8ILT2FpDKWVVDxKgQFMaMjrduWNvXHRBvvfGy0Nax_rgLSv3BFP15xKole3eofkcbVw")
 }
 
+func TestJwtES256NotExpired(t *testing.T) {
+	DoJwtTest(t, "/jwtes256", 200, "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJpc3MiOiJqb2UiLCJodHRwOi8vZXhhbXBsZS5jb20vaXNfcm9vdCI6dHJ1ZSwianRpIjoiZjUyYTIyMzItMzY4OS00OTM0LTg5OGMtYjhjMDkwNDNlNzM4IiwiaWF0IjoxNjA1NzMxMzE5LCJleHAiOjk0NzU4NzgzNTd9.mQMZXjhrpA_0PMF-0aO56Zj9G6pON-2DZ1R0dUn1xjECkolzBNx7J8JoQ9Ik8LfFivU4YPY7C7pkob6ygA0LCQ")
+}
+
+func TestJwtES256NotBeforeFails(t *testing.T) {
+	DoJwtTest(t, "/jwtes256", 401, "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJpc3MiOiJqb2UiLCJodHRwOi8vZXhhbXBsZS5jb20vaXNfcm9vdCI6dHJ1ZSwianRpIjoiNmIzMjlmYjgtNTM5Zi00NzgyLTlmNDEtMDIzZjM2M2EwNjFmIiwiaWF0IjoxNjA1NzMxNTAxLCJleHAiOjE2MDU3MzUxMDEsIm5iZiI6OTQ3NTg3ODM1N30.5QIBnoc_XtCnukv9Yisohk8IqEyW2KPX5Ixnw2h-wwDga8v00dsV4adFuuYAz0JDyZntDeR_SQFEbU69hh3yjw")
+}
+
+func TestJwtES256NotBeforeFailsAndExpired(t *testing.T) {
+	DoJwtTest(t, "/jwtes256", 401, "eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiJ9.eyJpc3MiOiJqb2UiLCJodHRwOi8vZXhhbXBsZS5jb20vaXNfcm9vdCI6dHJ1ZSwianRpIjoiYWFkNjMwNjctYzg4NS00NWViLWE1MGEtOTFiMThhOWFiYzJkIiwiaWF0IjoxNjA1NzM0MDE5LCJleHAiOjE1NzU4NzgzNTcsIm5iZiI6OTQ3NTg3ODM1N30.FDmGI21h2aOpng2cEx6dqiAUyynnOe_kilT9raTocYcVX2xGIcneu7LHZML9mh1MOHr8e4htxe5zhiZ90Pv9rg")
+}
+
+
 func DoJwtTest(t *testing.T, slug string, want int, forjwt string) {
 	client := &http.Client{}
 	req, _ := http.NewRequest("GET", fmt.Sprintf("http://localhost:8080%s/get", slug), nil)
