96e45f9f04eb21b01bb64ec96dfda2e982a4d1bb
simon.mittag <simonmittag@gmail.com>
2021-05-04 07:57:47 +1000

    more logging

--- integration/websocket_test.go
+++ integration/websocket_test.go
@@ -1,6 +1,7 @@
 package integration
 
 import (
+	"bytes"
 	"context"
 	"github.com/simonmittag/ws"
 	"github.com/simonmittag/ws/wsutil"
@@ -11,21 +12,30 @@ func TestWSConnectionEstablishedAndEchoMessageWithCleanExit(t *testing.T) {
 	con, _, _, e := ws.DefaultDialer.Dial(context.Background(), "ws://localhost:8080/websocket?n=1")
 	if e != nil {
 		t.Errorf("unable to connect to ws, cause: %v", e)
+		return
 	}
 
 	want := []byte("hello world")
 	e2 := wsutil.WriteClientMessage(con, ws.OpText, want)
 	if e2 != nil {
 		t.Errorf("unable to write ws message, cause: %v", e2)
+		return
 	}
 
 	msg, op, e3 := wsutil.ReadServerData(con)
 	if op != ws.OpText {
 		t.Errorf("opCode should be text, was: %v", op)
+		return
 	}
-	t.Logf("normal. received message: %v", string(msg))
+
 	if e3 != nil {
 		t.Errorf("unable to read back ws echo message, cause: %v", e3)
+		return
+	}
+	if bytes.Compare(want, msg) != 0 {
+		t.Errorf("sent %s, received wrong echo from server: %s", want, string(msg))
+	} else {
+		t.Logf("normal. sent and received message: %v", string(msg))
 	}
 
 	//so this is how you orderly close a WS connection in gobwas.
@@ -36,6 +46,7 @@ func TestWSConnectionEstablishedAndEchoMessageWithCleanExit(t *testing.T) {
 	e4 := ws.WriteFrame(con, cf)
 	if e4 != nil {
 		t.Errorf("unable to close ws protocol connection, cause: %v", e4)
+		return
 	}
 
 	e5 := con.Close()
