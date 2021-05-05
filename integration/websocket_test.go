3af1aaab7f810e7ddf2e578dc9eb7c7fe415f9f6
simon.mittag <simonmittag@gmail.com>
2021-05-06 08:39:34 +1000

    separate tests for downstream closing protocol, socket and both at the same time

--- integration/websocket_test.go
+++ integration/websocket_test.go
@@ -55,9 +55,72 @@ func TestWSConnectionEstablishedAndEchoMessageWithDownstreamExitClean(t *testing
 
 	e5 := con.Close()
 	if e5 != nil {
-		t.Errorf("unable to TCP socket connection, cause: %v", e5)
+		t.Errorf("unable to close TCP socket connection, cause: %v", e5)
 	}
 
+}
+
+func TestWSConnectionEstablishedAndEchoMessageWithDownstreamExitDirtyClosingJustProtocol(t *testing.T) {
+	con, _, _, e := ws.DefaultDialer.Dial(context.Background(), "ws://localhost:8080/websocket?n=1")
+	if e != nil {
+		t.Errorf("unable to connect to ws, cause: %v", e)
+		return
+	}
+
+	if !echoHelloWorld(t, con) {
+		return
+	}
+
+	//so this is how you orderly close a WS connection in gobwas.
+	cf := ws.NewCloseFrame(ws.NewCloseFrameBody(
+		ws.StatusNormalClosure, "unit test close requested",
+	))
+	cf = ws.MaskFrameInPlace(cf)
+	e4 := ws.WriteFrame(con, cf)
+	if e4 != nil {
+		t.Errorf("unable to close ws protocol connection, cause: %v", e4)
+		return
+	}
+
+	//in this case j8a closes the network connection after we forget to do so and we still should be unable to read any further.
+	msg, _, e6 := wsutil.ReadServerData(con)
+	if e6 == nil {
+		t.Errorf("error. should get read error after connection closed but none received, instead got msg: %v", msg)
+	} else {
+		if wce, wcet := e6.(wsutil.ClosedError); !wcet {
+			t.Errorf("error. we expect netop error here but got %s", wce)
+		} else {
+			t.Logf("normal. j8a closed connection with %s", e6)
+		}
+	}
+}
+
+func TestWSConnectionEstablishedAndEchoMessageWithDownstreamExitDirtyClosingJustSocket(t *testing.T) {
+	con, _, _, e := ws.DefaultDialer.Dial(context.Background(), "ws://localhost:8080/websocket?n=1")
+	if e != nil {
+		t.Errorf("error. unable to connect to ws, cause: %v", e)
+		return
+	}
+
+	if !echoHelloWorld(t, con) {
+		return
+	}
+
+	e5 := con.Close()
+	if e5 != nil {
+		t.Errorf("error. unable to close TCP socket connection, cause: %v", e5)
+	}
+
+	msg, _, e6 := wsutil.ReadServerData(con)
+	if e6 == nil {
+		t.Errorf("error. should get read error after connection closed but none received, instead got msg: %v", msg)
+	} else {
+		if netop, netopt := e6.(*net.OpError); !netopt {
+			t.Errorf("error. we expect netop error here but got %s", netop)
+		} else {
+			t.Logf("normal. j8a closed connection with %s", e6)
+		}
+	}
 }
 
 func echoHelloWorld(t *testing.T, con net.Conn) bool {
