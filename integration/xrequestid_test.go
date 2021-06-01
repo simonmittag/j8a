c3a51c3bc4cc0182f673d749b09be4026a39cb0a
simon.mittag <simonmittag@gmail.com>
2021-06-01 13:44:43 +1000

    tests for XRequestInfo

--- integration/xrequestid_test.go
+++ integration/xrequestid_test.go
@@ -56,3 +56,59 @@ func TestDoNotSupplyXRequestID(t *testing.T) {
 		t.Errorf("should have sent X-Request-ID header upstream, but sent only this %s", body)
 	}
 }
+
+func TestSupplyXRequestInfoUpstreamGzip(t *testing.T) {
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", "http://localhost:8080/mse6/gzip", nil)
+	req.Header.Add("Accept-Encoding", "identity")
+	req.Header.Add("X-Request-Info", "true")
+	req.Header.Add("X-Request-Id", "test1")
+	resp, err := client.Do(req)
+	if err != nil {
+		t.Errorf("error connecting to server, cause: %s", err)
+	}
+	if resp != nil && resp.Body != nil {
+		defer resp.Body.Close()
+	}
+
+	//must be case lowercase HTTP/2
+	got := resp.Header.Get("x-request-id")
+	want := "test1"
+	if got != want {
+		t.Errorf("server did not return supplied X-Request-Id, want %s, got %s", want, got)
+	}
+
+	body, _ := ioutil.ReadAll(resp.Body)
+	utf8 := string(body)
+	if !strings.Contains(utf8, "gzip endpoint") {
+		t.Errorf("should have sent decoded gzip respose with serverside request info but got this instead: %s", body)
+	}
+}
+
+func TestSupplyXRequestInfoUpstreamIdentity(t *testing.T) {
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", "http://localhost:8080/mse6/get", nil)
+	req.Header.Add("Accept-Encoding", "identity")
+	req.Header.Add("X-Request-Info", "true")
+	req.Header.Add("X-Request-Id", "test1")
+	resp, err := client.Do(req)
+	if err != nil {
+		t.Errorf("error connecting to server, cause: %s", err)
+	}
+	if resp != nil && resp.Body != nil {
+		defer resp.Body.Close()
+	}
+
+	//must be case lowercase HTTP/2
+	got := resp.Header.Get("x-request-id")
+	want := "test1"
+	if got != want {
+		t.Errorf("server did not return supplied X-Request-Id, want %s, got %s", want, got)
+	}
+
+	body, _ := ioutil.ReadAll(resp.Body)
+	utf8 := string(body)
+	if !strings.Contains(utf8, "get endpoint") {
+		t.Errorf("should have sent decoded gzip respose with serverside request info but got this instead: %s", body)
+	}
+}
