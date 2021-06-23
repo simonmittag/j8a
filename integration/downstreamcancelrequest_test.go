2c3c676c4e99f2049a5bbb51b20e9180316f2b12
simon.mittag <simonmittag@gmail.com>
2021-06-23 13:53:50 +1000

    499 tests for both before and after upstream request initialized

--- integration/downstreamcancelrequest_test.go
+++ integration/downstreamcancelrequest_test.go
@@ -3,8 +3,30 @@ package integration
 import (
 	"net"
 	"testing"
+	"time"
 )
 
+//you cannot observe the 499 the remote end tries to send because we hang up socket. check server logs
+func TestCancelDownstreamBeforeUpstreamConnection(t *testing.T) {
+	//step 1 we connect to j8a with net.dial
+	c, err := net.Dial("tcp", ":8081")
+	if err != nil {
+		t.Errorf("unable to connect to j8a server for integration test, cause: %v", err)
+		return
+	}
+
+	//step 2 we send headers and terminate HTTP message.
+	checkWrite(t, c, "GET /mse6/slowbody?wait=5 HTTP/1.1\r\n")
+	checkWrite(t, c, "Host: localhost:8081\r\n")
+	checkWrite(t, c, "User-Agent: integration\r\n")
+	checkWrite(t, c, "Accept: */*\r\n")
+	checkWrite(t, c, "\r\n")
+
+	c.Close()
+	t.Logf("normal. we are now closing the connection to J8a. it should drop it's connection to upstream immediately")
+}
+
+//you cannot observe the 499 the remote end tries to send because we hang up socket. check server logs
 func TestCancelDownstreamWhileUpstreamStillServing(t *testing.T) {
 	//step 1 we connect to j8a with net.dial
 	c, err := net.Dial("tcp", ":8081")
@@ -19,6 +41,9 @@ func TestCancelDownstreamWhileUpstreamStillServing(t *testing.T) {
 	checkWrite(t, c, "User-Agent: integration\r\n")
 	checkWrite(t, c, "Accept: */*\r\n")
 	checkWrite(t, c, "\r\n")
+
+	//wait 1 second before killing connection, enough for j8a to make upstream attempt.
+	time.Sleep(2 * time.Second)
 
 	c.Close()
 	t.Logf("normal. we are now closing the connection to J8a. it should drop it's connection to upstream immediately")
