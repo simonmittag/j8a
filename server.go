e8ad1550d3a0616b6ab71963d877ab6b38ba5691
simon.mittag <simonmittag@gmail.com>
2021-06-26 12:27:37 +1000

    bumped to v0.8.4

--- server.go
+++ server.go
@@ -18,7 +18,7 @@ import (
 //Version is the server version
 const Server string = "Server"
 
-var Version string = "v0.8.3"
+var Version string = "v0.8.4"
 
 //ID is a unique server ID
 var ID string = "unknown"
