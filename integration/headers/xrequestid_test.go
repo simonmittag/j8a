1bdc88575c885a6796fe816cf15f83c94ce04cbc
simon.mittag <simonmittag@gmail.com>
2022-03-05 09:00:49 +1100

    fixed broken test that was assuming decoded gzip

--- integration/headers/xrequestid_test.go
+++ integration/headers/xrequestid_test.go
@@ -82,7 +82,7 @@ func TestSupplyXRequestInfoUpstreamGzip(t *testing.T) {
 	body, _ := ioutil.ReadAll(resp.Body)
 	utf8 := string(*j8a.Gunzip(body))
 	if !strings.Contains(utf8, "gzip endpoint") {
-		t.Errorf("should have sent decoded gzip respose with serverside request info but got this instead: %s", body)
+		t.Errorf("should have sent gzip respose with serverside request info but got this instead: %s", body)
 	}
 }
 
@@ -108,9 +108,9 @@ func TestSupplyXRequestInfoUpstreamTinyGzip(t *testing.T) {
 	}
 
 	body, _ := ioutil.ReadAll(resp.Body)
-	utf8 := string(body)
+	utf8 := string(*j8a.Gunzip(body))
 	if !strings.Contains(utf8, "{}") {
-		t.Errorf("should have sent decoded gzip respose with tiny serverside request info but got this instead: %s", body)
+		t.Errorf("should have sent gzip respose with tiny serverside request info but got this instead: %s", body)
 	}
 }
 
