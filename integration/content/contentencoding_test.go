6c907f520f3332d173d837bf984c121e557706c7
simon.mittag <simonmittag@gmail.com>
2022-03-06 11:56:41 +1100

    Accept-Encoding tests for all handlers with empty string, star, and all supported encodings

--- integration/content/contentencoding_test.go
+++ integration/content/contentencoding_test.go
@@ -8,6 +8,18 @@ import (
 	"testing"
 )
 
+func TestNoAcceptEncodingOn404(t *testing.T) {
+	DownstreamAcceptEncodingContentEncodingHTTP11("", false, "identity", "/", t)
+}
+
+func TestEmptyAcceptEncodingOn404(t *testing.T) {
+	DownstreamAcceptEncodingContentEncodingHTTP11("", true, "identity", "/", t)
+}
+
+func TestStarAcceptEncodingOn404(t *testing.T) {
+	DownstreamAcceptEncodingContentEncodingHTTP11("*", true, "identity", "/", t)
+}
+
 func TestIdentityEncodingOn404(t *testing.T) {
 	DownstreamAcceptEncodingHTTP11("identity", "/", t)
 }
@@ -24,7 +36,67 @@ func TestDeflateEncodingOn404(t *testing.T) {
 	DownstreamAcceptEncodingHTTP11("deflate", "/", t)
 }
 
+func TestNoAcceptEncodingOnProxyHandler(t *testing.T) {
+	DownstreamAcceptEncodingContentEncodingHTTP11("", false, "gzip", "/mse6/get", t)
+}
+
+func TestEmptyAcceptEncodingOnProxyHandler(t *testing.T) {
+	DownstreamAcceptEncodingContentEncodingHTTP11("", true, "gzip", "/mse6/get", t)
+}
+
+func TestStarAcceptEncodingOnProxyHandler(t *testing.T) {
+	DownstreamAcceptEncodingContentEncodingHTTP11("*", true, "gzip", "/mse6/get", t)
+}
+
+func TestIdentityEncodingOnProxyHandler(t *testing.T) {
+	DownstreamAcceptEncodingHTTP11("identity", "/mse6/get", t)
+}
+
+func TestGzipEncodingOnProxyHandler(t *testing.T) {
+	DownstreamAcceptEncodingHTTP11("gzip", "/mse6/get", t)
+}
+
+func TestBrotliEncodingOnProxyHandler(t *testing.T) {
+	DownstreamAcceptEncodingHTTP11("br", "/mse6/get", t)
+}
+
+func TestDeflateEncodingOnProxyHandler(t *testing.T) {
+	DownstreamAcceptEncodingHTTP11("deflate", "/mse6/get", t)
+}
+
+func TestNoAcceptEncodingOnAboutHandler(t *testing.T) {
+	DownstreamAcceptEncodingContentEncodingHTTP11("", false, "identity", "/about", t)
+}
+
+func TestEmptyAcceptEncodingOnAboutHandler(t *testing.T) {
+	DownstreamAcceptEncodingContentEncodingHTTP11("", true, "identity", "/about", t)
+}
+
+func TestStarAcceptEncodingOnAboutHandler(t *testing.T) {
+	DownstreamAcceptEncodingContentEncodingHTTP11("*", true, "identity", "/about", t)
+}
+
+func TestIdentityEncodingOnAboutHandler(t *testing.T) {
+	DownstreamAcceptEncodingHTTP11("identity", "/about", t)
+}
+
+func TestGzipEncodingOnAboutHandler(t *testing.T) {
+	DownstreamAcceptEncodingHTTP11("gzip", "/about", t)
+}
+
+func TestBrotliEncodingOnAboutHandler(t *testing.T) {
+	DownstreamAcceptEncodingHTTP11("br", "/about", t)
+}
+
+func TestDeflateEncodingOnAboutHandler(t *testing.T) {
+	DownstreamAcceptEncodingHTTP11("deflate", "/about", t)
+}
+
 func DownstreamAcceptEncodingHTTP11(enc string, slug string, t *testing.T) {
+	DownstreamAcceptEncodingContentEncodingHTTP11(enc, true, enc, slug, t)
+}
+
+func DownstreamAcceptEncodingContentEncodingHTTP11(ae string, sendAEHeader bool, ce string, slug string, t *testing.T) {
 	//if this test fails, check the j8a configuration for connection.downstream.ReadTimeoutSeconds
 
 	//step 1 make a connection
@@ -36,15 +108,17 @@ func DownstreamAcceptEncodingHTTP11(enc string, slug string, t *testing.T) {
 	//step 2 we send headers and terminate http message so j8a sends request upstream.
 	integration.CheckWrite(t, c, fmt.Sprintf("GET %s HTTP/1.1\r\n", slug))
 	integration.CheckWrite(t, c, "Host: localhost:8080\r\n")
-	integration.CheckWrite(t, c, fmt.Sprintf("Accept-Encoding: %v\r\n", enc))
+	if sendAEHeader {
+		integration.CheckWrite(t, c, fmt.Sprintf("Accept-Encoding: %v\r\n", ae))
+	}
 	integration.CheckWrite(t, c, "\r\n")
 
-	//step 3 we read a response into buffer which returns 501
+	//step 3 we read a response into buffer which returns headers
 	buf := make([]byte, 128)
 	b, err2 := c.Read(buf)
 	t.Logf("normal. j8a responded with %v bytes", b)
-	if err2 != nil || !strings.Contains(string(buf), enc) {
-		t.Errorf("test failure. want response content encoding %s but got %s", enc, string(buf))
+	if err2 != nil || !strings.Contains(string(buf), ce) {
+		t.Errorf("test failure. accept encoding %s want response content encoding %s but got %s", ae, ce, string(buf))
 	} else {
 		t.Logf("normal. received response %s", string(buf))
 	}
