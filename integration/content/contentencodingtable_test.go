d04d8aa3c85154e5fb8354fdd1fc69a90bcd6b68
simon.mittag <simonmittag@gmail.com>
2022-03-28 10:49:18 +1100

    new table tests for all content encoding permutations. Tests HTTP response code, body and content encoding headers.
    also fixed an issue where golang was inserting Accept-Encoding: gzip automatically

--- integration/content/contentencodingtable_test.go
+++ integration/content/contentencodingtable_test.go
@@ -10,7 +10,8 @@ import (
 	"testing"
 )
 
-func TestContentEncodingAboutPermutations(t *testing.T) {
+//no upstream attempt made.
+func TestContentEncodingPermutationsOnAboutHandler(t *testing.T) {
 	tests := map[string]struct {
 		reqUrlSlug                      string
 		reqAcceptEncodingHeader         string
@@ -20,7 +21,7 @@ func TestContentEncodingAboutPermutations(t *testing.T) {
 		wantResVaryAcceptEncodingHeader bool
 		wantResBodyContent              string
 	}{
-		"noAcceptEncoding": {"/about",
+		"noAcceptEncodingSendsIdentity": {"/about",
 			"",
 			false,
 			200,
@@ -28,13 +29,178 @@ func TestContentEncodingAboutPermutations(t *testing.T) {
 			false,
 			"ServerID",
 		},
-		"emptyAcceptEncoding": {"/about",
+		"emptyAcceptEncodingSendsIdentity": {"/about",
 			"",
 			true,
 			200,
 			"identity",
 			false,
 			"ServerID",
+		},
+		"starAcceptEncodingSendsIdentity": {"/about",
+			"*",
+			true,
+			200,
+			"identity",
+			false,
+			"ServerID",
+		},
+		"starCommaGzipAcceptEncodingSendsIdentity": {"/about",
+			"*,gzip",
+			true,
+			200,
+			"identity",
+			false,
+			"ServerID",
+		},
+		"identityCommaGzipAcceptEncodingSendsIdentity": {"/about",
+			"identity,gzip",
+			true,
+			200,
+			"identity",
+			false,
+			"ServerID",
+		},
+		"gzipCommaIdentityAcceptEncodingSendsIdentity": {"/about",
+			"gzip,identity",
+			true,
+			200,
+			"identity",
+			false,
+			"ServerID",
+		},
+		"gzipCommaStarAcceptEncodingSendsIdentity": {"/about",
+			"gzip,*",
+			true,
+			200,
+			"identity",
+			false,
+			"ServerID",
+		},
+		"gzipAcceptEncodingSendsGzip": {"/about",
+			"gzip",
+			true,
+			200,
+			"gzip",
+			false,
+			"ServerID",
+		},
+		"deflateAcceptEncodingSends406ResponseCode": {"/about",
+			"deflate",
+			true,
+			406,
+			"identity",
+			false,
+			"406",
+		},
+		"unknownAcceptEncodingSends406ResponseCode": {"/about",
+			"unknown",
+			true,
+			406,
+			"identity",
+			false,
+			"406",
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			DownstreamContentEncodingFullIntegrity(tc.reqUrlSlug, tc.reqAcceptEncodingHeader, tc.reqSendAcceptEncodingHeader,
+				tc.wantResContentEncodingHeader, tc.wantResVaryAcceptEncodingHeader, tc.wantResBodyContent, tc.wantResStatusCode, t)
+		})
+	}
+}
+
+//no upstream attempt made
+func TestContentEncodingPermutationsOnStatusCodeResponseInProxyHandler(t *testing.T) {
+	tests := map[string]struct {
+		reqUrlSlug                      string
+		reqAcceptEncodingHeader         string
+		reqSendAcceptEncodingHeader     bool
+		wantResStatusCode               int
+		wantResContentEncodingHeader    string
+		wantResVaryAcceptEncodingHeader bool
+		wantResBodyContent              string
+	}{
+		"noAcceptEncodingSendsIdentity": {"/badslug",
+			"",
+			false,
+			404,
+			"identity",
+			false,
+			"404",
+		},
+		"emptyAcceptEncodingSendsIdentity": {"/badslug",
+			"",
+			true,
+			404,
+			"identity",
+			false,
+			"404",
+		},
+		"starAcceptEncodingSendsIdentity": {"/badslug",
+			"*",
+			true,
+			404,
+			"identity",
+			false,
+			"404",
+		},
+		"starCommaGzipAcceptEncodingSendsIdentity": {"/badslug",
+			"*,gzip",
+			true,
+			404,
+			"identity",
+			false,
+			"404",
+		},
+		"identityCommaGzipAcceptEncodingSendsIdentity": {"/badslug",
+			"identity,gzip",
+			true,
+			404,
+			"identity",
+			false,
+			"404",
+		},
+		"gzipCommaIdentityAcceptEncodingSendsIdentity": {"/badslug",
+			"gzip,identity",
+			true,
+			404,
+			"identity",
+			false,
+			"404",
+		},
+		"gzipCommaStarAcceptEncodingSendsIdentity": {"/badslug",
+			"gzip,*",
+			true,
+			404,
+			"identity",
+			false,
+			"404",
+		},
+		"gzipAcceptEncodingSendsGzip": {"/badslug",
+			"gzip",
+			true,
+			404,
+			"gzip",
+			false,
+			"404",
+		},
+		"deflateAcceptEncodingSends406ResponseCode": {"/badslug",
+			"deflate",
+			true,
+			406,
+			"identity",
+			false,
+			"406",
+		},
+		"unknownAcceptEncodingSends406ResponseCode": {"/badslug",
+			"unknown",
+			true,
+			406,
+			"identity",
+			false,
+			"406",
 		},
 	}
 
@@ -94,14 +260,11 @@ func DownstreamContentEncodingFullIntegrity(reqUrlSlug string, reqAcceptEncoding
 	if len(wantResBodyContent) > 0 {
 		if wantResContentEncodingHeader == "br" {
 			body = *j8a.BrotliDecode(body)
-		}
-		if wantResContentEncodingHeader == "gzip" {
+		} else if wantResContentEncodingHeader == "gzip" {
 			body = *j8a.Gunzip(body)
-		}
-		if wantResContentEncodingHeader == "deflate" {
+		} else if wantResContentEncodingHeader == "deflate" {
 			body, _ = ioutil.ReadAll(flate.NewReader(bytes.NewBuffer(body)))
-		}
-		if !strings.Contains(string(body), wantResBodyContent) {
+		} else if !strings.Contains(string(body), wantResBodyContent) {
 			t.Errorf("want body response %v, but got (decoded) %v", wantResBodyContent, string(body))
 		}
 	}
