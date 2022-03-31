cda6e1013b3866f6ededf401f900dce188332a9a
simon.mittag <simonmittag@gmail.com>
2022-03-31 15:01:15 +1100

    no longer copy vary headers from upstream to downstream response

--- proxyhandler.go
+++ proxyhandler.go
@@ -27,7 +27,7 @@ var httpClient HTTPClient
 
 //httpHeadersNoRewrite contains a list of headers that are not copied in either direction. they must be set by the
 //server or are ignored.
-var httpHeadersNoRewrite []string = []string{connectionS, date, contentLength, acceptEncoding, transferEncoding, server}
+var httpHeadersNoRewrite []string = []string{connectionS, date, contentLength, acceptEncoding, transferEncoding, server, varyS}
 
 //extract IPs for stdout. thread safe.
 var ipr iprex = iprex{}
