def5ce09df54e4fe13a58a8f78e014dcc1645d81
simon.mittag <simonmittag@gmail.com>
2023-03-25 14:32:36 +1100

    more test for special cases of OPTIONS, HEAD, TRACE and CONNECT.
    OPTIONS may receive full proxy response but can be without body.
    
    HEAD does not have a body but may have a content length header
    
    CONNECT never has a body, no matter what the upstream server does.
    
    TRACE has a body and is proxied as any other request

--- integration/method/method_test.go
+++ integration/method/method_test.go
@@ -3,7 +3,9 @@ package method
 import (
 	"bytes"
 	"encoding/json"
+	"io/ioutil"
 	"net/http"
+	"strconv"
 	"testing"
 )
 
@@ -12,62 +14,94 @@ func TestHttpMethods(t *testing.T) {
 		method       string
 		url          string
 		responseCode int
+		cl           int
 	}{
 		{
 			method:       "CONNECT",
 			url:          "/mse6/connect",
 			responseCode: 200,
+			cl:           0,
+		},
+		//we never proxy the body of a CONNECT request, see: https://httpwg.org/specs/rfc9110.html#CONNECT
+		{
+			method:       "CONNECT",
+			url:          "/mse6/connect?body=true",
+			responseCode: 200,
+			cl:           0,
 		},
 		{
 			method:       "GET",
 			url:          "/mse6/get",
 			responseCode: 200,
+			cl:           38,
 		},
 		{
 			method:       "HEAD",
-			url:          "/mse6/getorhead?cl=y",
+			url:          "/mse6/getorhead",
 			responseCode: 200,
+			cl:           0,
+		},
+		//does not send a body but may send content length header which needs proxying
+		{
+			method:       "HEAD",
+			url:          "/mse6/getorhead?cl=true",
+			responseCode: 200,
+			cl:           44,
 		},
 		{
 			method:       "POST",
 			url:          "/mse6/post",
 			responseCode: 201,
+			cl:           39,
 		},
 		{
 			method:       "PUT",
 			url:          "/mse6/put",
 			responseCode: 200,
+			cl:           38,
 		},
 		{
 			method:       "OPTIONS",
 			url:          "/mse6/options?code=200",
 			responseCode: 200,
+			cl:           0,
+		},
+		//if OPTIONS returns content we proxy it. The spec is not clear on this.
+		{
+			method:       "OPTIONS",
+			url:          "/mse6/options?code=200&body=true",
+			responseCode: 200,
+			cl:           42,
 		},
 		{
 			method:       "DELETE",
 			url:          "/mse6/delete",
 			responseCode: 204,
+			cl:           0,
 		},
 		{
 			method:       "PATCH",
 			url:          "/mse6/patch",
 			responseCode: 200,
+			cl:           40,
 		},
+		//TRACE content and headers are proxied.
 		{
 			method:       "TRACE",
 			url:          "/mse6/trace",
 			responseCode: 200,
+			cl:           40,
 		},
 	}
 
 	for _, test := range tests {
 		t.Run("TestWith"+test.method, func(t *testing.T) {
-			methodWithResponseCodeBody(t, test.method, test.url, test.responseCode)
+			methodWithResponseCodeBody(t, test.method, test.url, test.responseCode, test.cl)
 		})
 	}
 }
 
-func methodWithResponseCodeBody(t *testing.T, method string, url string, responseCode int) {
+func methodWithResponseCodeBody(t *testing.T, method string, url string, wantResponseCode int, wantCl int) {
 	client := &http.Client{}
 
 	url = "http://localhost:8080" + url
@@ -89,11 +123,24 @@ func methodWithResponseCodeBody(t *testing.T, method string, url string, respons
 		t.Errorf("error connecting to server for method %s, url %s, cause: %s", method, url, err)
 	}
 	if resp != nil && resp.Body != nil {
+		//don't try to read the body response for head request
+		if method != "HEAD" {
+			b, _ := ioutil.ReadAll(resp.Body)
+			if len(b) != wantCl {
+				t.Errorf("incorrect response body bytes, want %v, got %v", wantCl, len(b))
+			}
+		}
 		defer resp.Body.Close()
 	}
 
+	gotCl := resp.Header.Get("Content-Length")
+	gotCli, _ := strconv.Atoi(gotCl)
+	if wantCl != gotCli {
+		t.Errorf("incorrect content length response header, want %v, got %v", wantCl, gotCli)
+	}
+
 	gotCode := resp.StatusCode
-	if responseCode != gotCode {
-		t.Errorf("illegal response for method %s, url %s received %d want %d", method, url, gotCode, responseCode)
+	if wantResponseCode != gotCode {
+		t.Errorf("illegal response for method %s, url %s received %d want %d", method, url, gotCode, wantResponseCode)
 	}
 }
