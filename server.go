ca1aaec85aba12d99e8deee33849e570d0243676
simon.mittag <simonmittag@gmail.com>
2023-05-07 15:50:38 +1000

    GA release v1.0.0!

--- server.go
+++ server.go
@@ -22,7 +22,7 @@ import (
 // Version is the server version
 const Server string = "Server"
 
-var Version string = "v0.9.7"
+var Version string = "v1.0.0"
 
 // ID is a unique server ID
 var ID string = "unknown"
