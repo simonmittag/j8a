d583199216aed078a58cba1da88f3cf159246877
simon.mittag <simonmittag@gmail.com>
2023-06-22 22:28:22 +1000

    reset version number

--- server.go
+++ server.go
@@ -20,7 +20,7 @@ import (
 // Version is the server version
 const Server string = "Server"
 
-var Version string = "v1.0.2b7"
+var Version string = "v1.0.1"
 
 // ID is a unique server ID
 var ID string = "unknown"
